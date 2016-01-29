45d2c7b04aa774bbca4e2b82c27602deed380aa7
akkagi0416 <akkagi0416@gmail.com>
2016-01-30 02:02:00 +0900

    tweet time transform local time

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 require 'dotenv'
+require 'time'
 
 Dotenv.load
 
@@ -10,7 +11,16 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["ACCESS_SECRET"]
 end
 
-client.user_timeline("akkagi0416", { count: 5 } ).each do |timeline|
-  tweet = client.status(timeline.id)
-  puts tweet.created_at
+tweets = client.user_timeline("akkagi0416", { count: 200 } )
+
+tweet_counts = []
+(0..23).each {|i| tweet_counts[i] = 0 }
+
+tweets.each do |tweet|
+  tweet_counts[tweet.created_at.getlocal.hour] += 1 # 日本時間に変更
 end
+
+(0..23).each do |i|
+  printf "%02d %s\n", i, "#" * tweet_counts[i] 
+end
+
