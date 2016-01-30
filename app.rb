26f3379a0f7688179addb0881d4eed2e4384114e
akkagi0416 <akkagi0416@gmail.com>
2016-01-30 13:59:07 +0900

    make class

--- app.rb
+++ app.rb
@@ -5,25 +5,41 @@ require 'sinatra'
 
 Dotenv.load
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["CONSUMER_KEY"]
-  config.consumer_secret     = ENV["CONSUMER_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_SECRET"]
+class App
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_SECRET"]
+    end
+    @screen_name  = nil
+    @tweet_counts = Array.new(24, 0)
+  end
+
+  def get_tweet_counts(screen_name = 'akkagi0416')
+    @screen_name = screen_name
+    tweets = @client.user_timeline(@screen_name, { count: 200 } )
+    tweets.each do |tweet|
+      @tweet_counts[tweet.created_at.getlocal.hour] += 1 # 日本時間に変更
+    end
+    @tweet_counts
+  end
+
+  # tweet_counts = []
+  # (0..23).each {|i| tweet_counts[i] = 0 }
+
+  # tweets.each do |tweet|
+  #   tweet_counts[tweet.created_at.getlocal.hour] += 1 # 日本時間に変更
+  # end
+
+  # (0..23).each do |i|
+  #   printf "%02d %s\n", i, "#" * tweet_counts[i] 
+  # end
 end
 
-tweets = client.user_timeline("akkagi0416", { count: 200 } )
-
-tweet_counts = []
-(0..23).each {|i| tweet_counts[i] = 0 }
-
-tweets.each do |tweet|
-  tweet_counts[tweet.created_at.getlocal.hour] += 1 # 日本時間に変更
-end
-
-# (0..23).each do |i|
-#   printf "%02d %s\n", i, "#" * tweet_counts[i] 
-# end
+app = App.new
+p app.get_tweet_counts
 
 # Web part
 get '/' do
